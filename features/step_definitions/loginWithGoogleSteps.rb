0a2c7458bc94689e74e99c98ab96c120e2f49fb7
Jav4nsJuice <javans.ferrel@gmail.com>
2022-11-28 19:59:44 -0400

    Login with Google features added in R,L and C roles

--- features/step_definitions/loginWithGoogleSteps.rb
+++ features/step_definitions/loginWithGoogleSteps.rb
@@ -2,4 +2,20 @@
 When(/^I press the button Iniciar con Google$/) do
     xpath = '//*[@id="root"]/div[2]/div[1]/div/div[2]/div[2]/div/form/div/div[3]/div/button'
     find(:xpath, xpath).click
+    sleep 1
+end
+
+# And I select the account registered with role Volunteer
+When(/^I select the account registered with role Volunteer$/) do
+    page.driver.browser.switch_to.alert.accept
+end
+
+# And I select the account registered with role Leader
+When(/^I select the account registered with role Leader$/) do
+    page.driver.browser.switch_to.alert.accept
+end
+
+# And I select the account registered with role CoreTeam
+When(/^I select the account registered with role CoreTeam$/) do
+    page.driver.browser.switch_to.alert.accept
 end
